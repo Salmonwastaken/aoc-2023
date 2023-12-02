6fc0a369d95e51736c3b079d7b7d3fdac149b14a
gerding.j <gerding.j@tkppensioen.nl>
2023-12-02 13:47:49 +0100

    day2-1 without regexp

--- day2/main.go
+++ day2/main.go
@@ -5,66 +5,57 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"reflect"
-	"regexp"
 	"strconv"
+	"strings"
 )
-
-type cubes struct {
-	red   int
-	green int
-	blue  int
-}
 
 func main() {
 	part1()
 }
 
 func part1() {
-	availableCubes := cubes{red: 12, green: 13, blue: 14}
-	var totalNumber int
+	var possibleAmount int
+	var gameNumber int
 
 	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
-
-	re := regexp.MustCompile(`^Game (?P<game>\d+)|(?P<red>\d+) red|(?P<green>\d+) green|(?P<blue>\d+) blue`)
 	scanner := bufio.NewScanner(file)
-lineLoop:
-	for scanner.Scan() {
-		out := re.FindAllStringSubmatch(scanner.Text(), -1)
-		gameNumber, err := strconv.Atoi(out[0][1])
-		if err != nil {
-			log.Fatal(err)
-		}
 
-		for _, match := range out {
-			for _, c := range []string{"red", "green", "blue"} {
-				if match[re.SubexpIndex(c)] == "" {
-					continue
-				}
-				number, err := strconv.Atoi(match[re.SubexpIndex(c)])
+	for scanner.Scan() {
+		gameNumber++
+		l := strings.Split(scanner.Text(), ": ")[1]
+		p := strings.Split(l, "; ")
+
+		possible := true
+		for _, g := range p {
+			c := strings.Split(g, ", ")
+			for _, e := range c {
+				n := strings.Split(e, " ")
+				num, err := strconv.Atoi(n[0])
 				if err != nil {
 					log.Fatal(err)
 				}
-				if number > getField(&availableCubes, c) {
-					continue lineLoop
+				if n[1] == "blue" && num > 14 {
+					possible = false
+				}
+				if n[1] == "green" && num > 13 {
+					possible = false
+				}
+				if n[1] == "red" && num > 12 {
+					possible = false
 				}
 			}
 		}
-		totalNumber += gameNumber
+		if possible {
+			possibleAmount += gameNumber
+		}
 	}
-	fmt.Println(totalNumber)
+	fmt.Println(possibleAmount)
 }
 
 func part2() {
 	return
-}
-
-func getField(cube *cubes, c string) int {
-	r := reflect.ValueOf(cube)
-	f := reflect.Indirect(r).FieldByName(c)
-	return int(f.Int())
 }
