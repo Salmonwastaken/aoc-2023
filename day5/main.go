81e0b7ea582dd62e3b21ccc44463e46ead434d83
gerding.j <gerding.j@tkppensioen.nl>
2023-12-06 10:50:40 +0100

    5-1&5-2 done

--- day5/main.go
+++ day5/main.go
@@ -9,7 +9,13 @@ import (
 	"strings"
 )
 
-type almaMap map[string]map[int]int
+type alma struct {
+	sourceRangeStart      int
+	destinationRangeStart int
+	allowedRange          int
+}
+
+type almaMap map[string][]alma
 
 func processSeeds(seeds []int, m almaMap) int {
 	answer := 0
@@ -25,6 +31,30 @@ func processSeeds(seeds []int, m almaMap) int {
 	return answer
 }
 
+func processSeeds2(seeds []int, m almaMap) int {
+	answer := 0
+
+	start := true
+
+	for k := range seeds {
+		if start {
+			for i := seeds[k]; i <= seeds[k]+seeds[k+1]-1; i++ {
+				location := transformSeed(i, m)
+
+				if answer == 0 || location < answer {
+					answer = location
+				}
+			}
+			start = false
+		} else {
+			start = true
+			continue
+		}
+	}
+
+	return answer
+}
+
 func transformSeed(seed int, m almaMap) int {
 	stages := []string{
 		"seed-to-soil", "soil-to-fertilizer", "fertilizer-to-water",
@@ -34,8 +64,13 @@ func transformSeed(seed int, m almaMap) int {
 
 	location := seed
 	for _, stage := range stages {
-		if val, ok := m[stage][location]; ok {
-			location = val
+		for _, v := range m[stage] {
+			offset := 0
+			if (location >= v.sourceRangeStart) && (location <= v.sourceRangeStart+v.allowedRange-1) {
+				offset = (location - v.sourceRangeStart)
+				location = v.destinationRangeStart + offset
+				break
+			}
 		}
 	}
 
@@ -43,6 +78,11 @@ func transformSeed(seed int, m almaMap) int {
 }
 
 func main() {
+	// fmt.Println(part1())
+	fmt.Println(part2())
+}
+
+func part2() int {
 	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -80,7 +120,7 @@ func main() {
 		if newCategory {
 			// This strips the word map:
 			category = currentLine[:strings.Index(currentLine, " map:")]
-			m[category] = make(map[int]int)
+			m[category] = []alma{}
 			newCategory = false
 			continue
 		}
@@ -96,16 +136,78 @@ func main() {
 			}
 			lineNumbers[k] = number
 		}
+		m[category] = append(m[category], alma{
+			destinationRangeStart: lineNumbers[0],
+			sourceRangeStart:      lineNumbers[1],
+			allowedRange:          lineNumbers[2],
+		})
+	}
 
-		destinationRangeStart := lineNumbers[0]
-		sourceRangeStart := lineNumbers[1]
+	answer := processSeeds2(seeds, m)
+	return answer
+}
 
-		totalValues := lineNumbers[2]
-		for i := 0; i < totalValues; i++ {
-			m[category][sourceRangeStart+i] = destinationRangeStart + i
+func part1() int {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	var newCategory bool
+	var category string
+	var seeds []int
+	m := make(almaMap, 7)
+	// Parse every line to build a map of the almanac file
+	for scanner.Scan() {
+		currentLine := scanner.Text()
+
+		// If empty newline, a new category has been reached
+		if currentLine == "" {
+			newCategory = true
+			continue
 		}
+
+		// Special handling for seeds
+		if strings.Contains(currentLine, "seeds: ") {
+			for _, v := range strings.Split(currentLine, " ")[1:] {
+				number, err := strconv.Atoi(v)
+				if err != nil {
+					log.Fatal(err)
+				}
+				seeds = append(seeds, number)
+			}
+			continue
+		}
+
+		// If previous line was empty newline, make a new category
+		if newCategory {
+			// This strips the word map:
+			category = currentLine[:strings.Index(currentLine, " map:")]
+			m[category] = []alma{}
+			newCategory = false
+			continue
+		}
+
+		// Start building the category maps
+		var lineNumbers [3]int
+
+		// Turn available ranges into numbers
+		for k, v := range strings.Split(currentLine, " ") {
+			number, err := strconv.Atoi(v)
+			if err != nil {
+				log.Fatal(err)
+			}
+			lineNumbers[k] = number
+		}
+		m[category] = append(m[category], alma{
+			destinationRangeStart: lineNumbers[0],
+			sourceRangeStart:      lineNumbers[1],
+			allowedRange:          lineNumbers[2],
+		})
 	}
 
 	answer := processSeeds(seeds, m)
-	fmt.Println(answer)
+	return answer
 }
