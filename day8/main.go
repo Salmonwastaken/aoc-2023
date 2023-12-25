81bd34ae798eea2ed2ac472ecfe4632c71e0c8b6
Salmonwastaken <34370061+Salmonwastaken@users.noreply.github.com>
2023-12-25 12:50:39 +0100

    Day 8-2 finished

--- day8/main.go
+++ day8/main.go
@@ -69,6 +69,109 @@ func part1() {
 	fmt.Println(stepCounter)
 }
 
+func generateAddressBook(scanner *bufio.Scanner) ([]string, []string, addresses) {
+	var instructionSet []string
+	addressBook := make(addresses)
+	var startingPoints []string
+
+	for scanner.Scan() {
+		currentLine := scanner.Text()
+		// Skip empty lines
+		if currentLine == "" {
+			continue
+		}
+
+		// First line doesn't contain the = symbol, but should contain instructions
+		if !strings.Contains(currentLine, "=") {
+			instructionSet = strings.Split(currentLine, "")
+			continue
+		}
+
+		reAddress := regexp.MustCompile(`\w{3}`)
+		splitAddress := reAddress.FindAllString(currentLine, -1)
+
+		addressBook[splitAddress[0]] = &directions{
+			left:  splitAddress[1],
+			right: splitAddress[2],
+		}
+
+		reStart := regexp.MustCompile(`.{2}A{1}`)
+		splitStart := reStart.FindString(currentLine)
+
+		if splitStart != "" {
+			startingPoints = append(startingPoints, splitStart)
+		}
+	}
+	return instructionSet, startingPoints, addressBook
+}
+
+// TODO: Try and implement this:
+// https://stackoverflow.com/questions/3154454/what-is-the-most-efficient-way-to-calculate-the-least-common-multiple-of-two-int#3154503
+// !! For each starting A node you calculate only the number of steps between the first Z node and when it gets back to that same Z node !!
+// you can then get the LCM between them to determine when they will end up in the same iteration or cycle with some MATHs.
+func part2() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	instructionSet, startingPoints, addressBook := generateAddressBook(scanner)
+
+	// Verify end point
+	reEnd := regexp.MustCompile(`.{2}Z{1}`)
+
+	steps := make([]int, 2)
+
+	for _, p := range startingPoints {
+		var stepCounter int
+
+		currentLocation := p
+		for {
+			directionOffset := stepCounter % len(instructionSet)
+			if instructionSet[directionOffset] == "L" {
+				currentLocation = addressBook[currentLocation].left
+			} else {
+				currentLocation = addressBook[currentLocation].right
+			}
+			stepCounter++
+			if reEnd.MatchString(currentLocation) {
+				steps = append(steps, stepCounter)
+				break
+			}
+		}
+	}
+	// First two are empty for some reason, cba to figure out why.
+	// So we'll just skip them
+	fmt.Println(LCM(steps[2], steps[3], steps[4:]...))
+}
+
+// GCD and LCM functions taken from: https://siongui.github.io/2017/06/03/go-find-lcm-by-gcd/#footnote-1
+// I'm no good at math
+
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
+}
+
 func main() {
 	part1()
+	part2()
 }
